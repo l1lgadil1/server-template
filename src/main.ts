aac6ed9a0a3b2ea57580545546780d36bef2de01
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2023-11-29 14:16:15 +0600

    init

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: false });
+
+  const config = new DocumentBuilder()
+    .setTitle('Server ')
+    .setDescription('Server API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors({ credentials: true, origin: true });
+
+  await app.listen(7777);
+}
+bootstrap();
